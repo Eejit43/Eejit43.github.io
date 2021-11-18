701b7910b721b0875de1976d07e9bedcb5bd2f75
Eejit <76887639+Eejit43@users.noreply.github.com>
2021-11-18 17:15:49 -0500

    Far better time info

--- scripts/time.js
+++ scripts/time.js
@@ -1,10 +1,22 @@
-timer();
-function timer(){
-    var currentTime = new Date()
-    var fullhours = currentTime.getHours()
-    var hours = ((fullhours + 11) % 12 + 1)
-    var minutes = currentTime.getMinutes()
-    var sec = currentTime.getSeconds()
+time();
+function time(){
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    var currentTime = new Date();
+    var day = days[currentTime.getDay()];
+    var date = currentTime.getDay();
+    var month = months[currentTime.getMonth()];
+    var monthnumber = (currentTime.getMonth() + 1);
+    var year = currentTime.getFullYear();
+    var shortyear = year.toString().substr(-2);
+    var fullhours = currentTime.getHours();
+    var hours = ((fullhours + 11) % 12 + 1);
+    var minutes = currentTime.getMinutes();
+    var sec = currentTime.getSeconds();
+    var msec = currentTime.getMilliseconds();
+    var unixtime = currentTime.getTime();
+    var timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    var timeoffset = (currentTime.getTimezoneOffset() / 60);
     if (minutes < 10){
         minutes = "0" + minutes
     }
@@ -14,8 +26,8 @@ function timer(){
 
     var suffix = fullhours >= 12 ? "PM":"AM";
 
-    var finaltime = hours + ":" + minutes + ":" + sec + " " + suffix;
+    var finaltime = "Current Time: " + hours + ":" + minutes + ":" + sec + " " + suffix + "<br>Current Date: " + day + ", " + month + " " + date + ", " + year + " (" + monthnumber + "/" + date + "/" + shortyear + ")" + "<br>Current UNIX Epoch Time: " + unixtime + "<br>Timezone: " + timezone + " (UTC-" + timeoffset + ")";
 
     document.getElementById('curtime').innerHTML = finaltime;
-    setTimeout(timer,1000);
+    setTimeout(time,100);
     }
