f2ff8113b148831d879cca93ecccf7ffbc32a375
Eejit <76887639+Eejit43@users.noreply.github.com>
2021-11-18 15:27:12 -0500

    remove old js

--- js/index.js
+++
@@ -1,9 +0,0 @@
-$("header .hamburger").click(() => {
-	$("header nav").toggleClass("active");
-	if ($("header nav").hasClass("active"))
-		setTimeout(() => {
-			$("header .hamburger").toggleClass("active");
-		}, 100);
-	 else
-		$("header .hamburger").toggleClass("active");
-});
