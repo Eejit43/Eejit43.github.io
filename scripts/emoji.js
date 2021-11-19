ca5c2ef61099195c11c5047a0b3dead962b2c89f
Eejit <76887639+Eejit43@users.noreply.github.com>
2021-11-19 13:18:13 -0500

    retry emoji twemoji script after 100 ms

--- scripts/emoji.js
+++ scripts/emoji.js
@@ -2,4 +2,10 @@ window.onload = function() {
     twemoji.parse(document.body,
         {folder: 'svg', ext: '.svg'}
         );
+
+    setTimeout(function(){
+    twemoji.parse(document.body,
+        {folder: 'svg', ext: '.svg'}
+        );
+}, 100);
 }
