de639c1dad1cf123a56af487391c9b6579169b83
Eejit <76887639+Eejit43@users.noreply.github.com>
2021-11-21 22:03:26 -0500

    added more countdowns

--- scripts/countdowns.js
+++ scripts/countdowns.js
@@ -1,3 +1,177 @@
+var valentines = setInterval(function () {
+
+  var countdownDate = new Date("February 14, 2022 00:00:00").getTime();
+
+  var daysfinal = null;
+  var hoursfinal = null;
+  var minutesfinal = null;
+  var secondsfinal = null;
+
+  var daysfinalsuffix = null;
+  var hoursfinalsuffix = null;
+  var minutesfinalsuffix = null;
+
+  var curtime = new Date().getTime();
+
+  var distance = countdownDate - curtime;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  if (hours == 0 && minutes == 0 && seconds == 0) {
+    var daysfinalsuffix = "";
+  } else {
+    var daysfinalsuffix = ", ";
+  }
+
+  if (days == 1) {
+    var daysfinal = days + " day" + daysfinalsuffix;
+
+  } else if (days == 0) {
+    var daysfinal = "";
+  } else {
+    var daysfinal = days + " days" + daysfinalsuffix;
+  }
+
+  if (minutes == 0 && seconds == 0) {
+    var hoursfinalsuffix = "";
+  } else {
+    var hoursfinalsuffix = ", ";
+  }
+
+  if (hours == 1) {
+    var hoursfinal = hours + " hour" + hoursfinalsuffix;
+
+  } else if (hours == 0) {
+    var hoursfinal = "";
+  } else {
+    var hoursfinal = hours + " hours" + hoursfinalsuffix;
+  }
+
+  if (seconds == 0) {
+    var minutesfinalsuffix = "";
+  } else {
+    var minutesfinalsuffix = ", ";
+  }
+
+  if (minutes == 1) {
+    var minutesfinal = minutes + " minute" + minutesfinalsuffix;
+
+  } else if (minutes == 0) {
+    var minutesfinal = "";
+  } else {
+    var minutesfinal = minutes + " minutes" + minutesfinalsuffix;
+  }
+
+  if (seconds == 1) {
+    var secondsfinal = seconds + " second";
+
+  } else if (seconds == 0) {
+    var secondsfinal = "";
+  } else {
+    var secondsfinal = seconds + " seconds";
+  }
+
+  document.getElementById("valentines").innerHTML = daysfinal + hoursfinal +
+    minutesfinal + secondsfinal;
+
+
+  if (distance < 0) {
+    clearInterval(countdown);
+    document.getElementById("valentines").innerHTML = "<span style='color:#FF5555;'>This event has already happened!</span>";
+  }
+}, 100);
+
+
+var patricks = setInterval(function () {
+
+  var countdownDate = new Date("March 17, 2022 00:00:00").getTime();
+
+  var daysfinal = null;
+  var hoursfinal = null;
+  var minutesfinal = null;
+  var secondsfinal = null;
+
+  var daysfinalsuffix = null;
+  var hoursfinalsuffix = null;
+  var minutesfinalsuffix = null;
+
+  var curtime = new Date().getTime();
+
+  var distance = countdownDate - curtime;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  if (hours == 0 && minutes == 0 && seconds == 0) {
+    var daysfinalsuffix = "";
+  } else {
+    var daysfinalsuffix = ", ";
+  }
+
+  if (days == 1) {
+    var daysfinal = days + " day" + daysfinalsuffix;
+
+  } else if (days == 0) {
+    var daysfinal = "";
+  } else {
+    var daysfinal = days + " days" + daysfinalsuffix;
+  }
+
+  if (minutes == 0 && seconds == 0) {
+    var hoursfinalsuffix = "";
+  } else {
+    var hoursfinalsuffix = ", ";
+  }
+
+  if (hours == 1) {
+    var hoursfinal = hours + " hour" + hoursfinalsuffix;
+
+  } else if (hours == 0) {
+    var hoursfinal = "";
+  } else {
+    var hoursfinal = hours + " hours" + hoursfinalsuffix;
+  }
+
+  if (seconds == 0) {
+    var minutesfinalsuffix = "";
+  } else {
+    var minutesfinalsuffix = ", ";
+  }
+
+  if (minutes == 1) {
+    var minutesfinal = minutes + " minute" + minutesfinalsuffix;
+
+  } else if (minutes == 0) {
+    var minutesfinal = "";
+  } else {
+    var minutesfinal = minutes + " minutes" + minutesfinalsuffix;
+  }
+
+  if (seconds == 1) {
+    var secondsfinal = seconds + " second";
+
+  } else if (seconds == 0) {
+    var secondsfinal = "";
+  } else {
+    var secondsfinal = seconds + " seconds";
+  }
+
+  document.getElementById("patricks").innerHTML = daysfinal + hoursfinal +
+    minutesfinal + secondsfinal;
+
+
+  if (distance < 0) {
+    clearInterval(countdown);
+    document.getElementById("patricks").innerHTML = "<span style='color:#FF5555;'>This event has already happened!</span>";
+  }
+}, 100);
+
+
 var easter = setInterval(function () {
 
   var countdownDate = new Date("April 17, 2022 00:00:00").getTime();
@@ -81,6 +255,180 @@ var easter = setInterval(function () {
   if (distance < 0) {
     clearInterval(countdown);
     document.getElementById("easter").innerHTML = "<span style='color:#FF5555;'>This event has already happened!</span>";
+  }
+}, 100);
+
+
+var independence = setInterval(function () {
+
+  var countdownDate = new Date("July 4, 2022 00:00:00").getTime();
+
+  var daysfinal = null;
+  var hoursfinal = null;
+  var minutesfinal = null;
+  var secondsfinal = null;
+
+  var daysfinalsuffix = null;
+  var hoursfinalsuffix = null;
+  var minutesfinalsuffix = null;
+
+  var curtime = new Date().getTime();
+
+  var distance = countdownDate - curtime;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  if (hours == 0 && minutes == 0 && seconds == 0) {
+    var daysfinalsuffix = "";
+  } else {
+    var daysfinalsuffix = ", ";
+  }
+
+  if (days == 1) {
+    var daysfinal = days + " day" + daysfinalsuffix;
+
+  } else if (days == 0) {
+    var daysfinal = "";
+  } else {
+    var daysfinal = days + " days" + daysfinalsuffix;
+  }
+
+  if (minutes == 0 && seconds == 0) {
+    var hoursfinalsuffix = "";
+  } else {
+    var hoursfinalsuffix = ", ";
+  }
+
+  if (hours == 1) {
+    var hoursfinal = hours + " hour" + hoursfinalsuffix;
+
+  } else if (hours == 0) {
+    var hoursfinal = "";
+  } else {
+    var hoursfinal = hours + " hours" + hoursfinalsuffix;
+  }
+
+  if (seconds == 0) {
+    var minutesfinalsuffix = "";
+  } else {
+    var minutesfinalsuffix = ", ";
+  }
+
+  if (minutes == 1) {
+    var minutesfinal = minutes + " minute" + minutesfinalsuffix;
+
+  } else if (minutes == 0) {
+    var minutesfinal = "";
+  } else {
+    var minutesfinal = minutes + " minutes" + minutesfinalsuffix;
+  }
+
+  if (seconds == 1) {
+    var secondsfinal = seconds + " second";
+
+  } else if (seconds == 0) {
+    var secondsfinal = "";
+  } else {
+    var secondsfinal = seconds + " seconds";
+  }
+
+  document.getElementById("independence").innerHTML = daysfinal + hoursfinal +
+    minutesfinal + secondsfinal;
+
+
+  if (distance < 0) {
+    clearInterval(countdown);
+    document.getElementById("independence").innerHTML = "<span style='color:#FF5555;'>This event has already happened!</span>";
+  }
+}, 100);
+
+
+var halloween = setInterval(function () {
+
+  var countdownDate = new Date("October 31, 2022 00:00:00").getTime();
+
+  var daysfinal = null;
+  var hoursfinal = null;
+  var minutesfinal = null;
+  var secondsfinal = null;
+
+  var daysfinalsuffix = null;
+  var hoursfinalsuffix = null;
+  var minutesfinalsuffix = null;
+
+  var curtime = new Date().getTime();
+
+  var distance = countdownDate - curtime;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  if (hours == 0 && minutes == 0 && seconds == 0) {
+    var daysfinalsuffix = "";
+  } else {
+    var daysfinalsuffix = ", ";
+  }
+
+  if (days == 1) {
+    var daysfinal = days + " day" + daysfinalsuffix;
+
+  } else if (days == 0) {
+    var daysfinal = "";
+  } else {
+    var daysfinal = days + " days" + daysfinalsuffix;
+  }
+
+  if (minutes == 0 && seconds == 0) {
+    var hoursfinalsuffix = "";
+  } else {
+    var hoursfinalsuffix = ", ";
+  }
+
+  if (hours == 1) {
+    var hoursfinal = hours + " hour" + hoursfinalsuffix;
+
+  } else if (hours == 0) {
+    var hoursfinal = "";
+  } else {
+    var hoursfinal = hours + " hours" + hoursfinalsuffix;
+  }
+
+  if (seconds == 0) {
+    var minutesfinalsuffix = "";
+  } else {
+    var minutesfinalsuffix = ", ";
+  }
+
+  if (minutes == 1) {
+    var minutesfinal = minutes + " minute" + minutesfinalsuffix;
+
+  } else if (minutes == 0) {
+    var minutesfinal = "";
+  } else {
+    var minutesfinal = minutes + " minutes" + minutesfinalsuffix;
+  }
+
+  if (seconds == 1) {
+    var secondsfinal = seconds + " second";
+
+  } else if (seconds == 0) {
+    var secondsfinal = "";
+  } else {
+    var secondsfinal = seconds + " seconds";
+  }
+
+  document.getElementById("halloween").innerHTML = daysfinal + hoursfinal +
+    minutesfinal + secondsfinal;
+
+
+  if (distance < 0) {
+    clearInterval(countdown);
+    document.getElementById("halloween").innerHTML = "<span style='color:#FF5555;'>This event has already happened!</span>";
   }
 }, 100);
 
