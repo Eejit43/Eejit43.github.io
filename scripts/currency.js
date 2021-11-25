d9fcb64d2a2d106fa340cf2784916dd5150561be
Eejit <76887639+Eejit43@users.noreply.github.com>
2021-11-24 19:56:26 -0500

    currency exchange (basic setup)

---
+++ scripts/currency.js
@@ -0,0 +1,48 @@
+//More info about api data on https://app.abstractapi.com/api/exchange-rates/documentation
+
+let today = new Date();
+let yesterday = new Date(today);
+
+yesterday.setDate(yesterday.getDate() - 1);
+
+today.toDateString();
+yesterday.toDateString();
+
+let prevdate = yesterday.getFullYear() + "-" + (yesterday.getMonth() + 1) + "-" + yesterday.getDate()
+
+$.when(
+  $.getJSON("https://exchange-rates.abstractapi.com/v1/live/?api_key=dc0955a3bba74d768ea3bd6a48bc522e&base=USD") //,
+  //$.getJSON(`https://exchange-rates.abstractapi.com/v1/historical/?api_key=dc0955a3bba74d768ea3bd6a48bc522e&base=USD&target=EUR,GBP,CAD,BTC,DOGE&date=${prevdate}`)
+).done(function (curdata /*, prevdata*/ ) {
+  // Current rates
+  rates = JSON.stringify(curdata/*[0]*/.exchange_rates);
+  rp = JSON.parse(rates);
+  usdcad = rp.CAD;
+  usdeur = rp.EUR;
+  usdgbp = rp.GBP;
+  usdbtc = rp.BTC;
+  usddoge = rp.DOGE;
+  // Prev rates
+  /*prevrates = JSON.stringify(prevdata[0].exchange_rates);
+  prevrp = JSON.parse(prevrates);
+  prevusdcad = prevrp.CAD;
+  prevusdeur = prevrp.EUR;
+  prevusdgbp = prevrp.GBP;
+  prevusdbtc = prevrp.BTC;
+  prevusddoge = prevrp.DOGE;
+
+  if (usdcad > prevusdcad) {
+    console.log("CAD exchange rate is rising!")
+  } else if (usdcad < prevusdcad) {
+    console.log("CAD exchange rate is decreasing!")
+  } else {
+    console.log("CAD exchange rate is constant.")
+  }*/
+
+  document.getElementById('usdcad').innerHTML = usdcad;
+  document.getElementById('usdeur').innerHTML = usdeur;
+  document.getElementById('usdgbp').innerHTML = usdgbp;
+  document.getElementById('usdbtc').innerHTML = usdbtc;
+  document.getElementById('usddoge').innerHTML = usddoge;
+  document.getElementById('reloadprompt').innerHTML = "";
+});
--- scripts/ip.js
+++ scripts/ip.js
@@ -1,4 +1,4 @@
-//More info about api data on https://app.abstractapi.com/api/ip-geolocation
+//More info about api data on https://app.abstractapi.com/api/ip-geolocation/documentation
 
 var output;
 
@@ -24,4 +24,5 @@ $.getJSON("https://ipgeolocation.abstractapi.com/v1/?api_key=7dcea861f91a44579ce
        output = "<span class='tooltip-text tooltip-right' data-tooltip='Internet Protocol'>IP</span> Address: " + ip + "<br><span class='tooltip-text tooltip-right' data-tooltip='Internet Service Provider'>ISP</span>: " + isp + "<br>City: " + city + "<br>Region: " + region + " (" + region_code + ")" + "<br>Country: " + country + " (" + country_code + ") " + flagemoji + "<br>Continent: " + continent + " (" + continent_code + ")" + "<br>Latitude (north-south): " + latitude + "<br>Longitude (east-west): " + longitude;
 
        document.getElementById('ip').innerHTML = output;
+       document.getElementById('reloadprompt').innerHTML = "";
 })
