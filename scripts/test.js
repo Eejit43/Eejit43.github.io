e57aab1d747a257a0d309581dac99fdd0fac7081
Eejit <76887639+Eejit43@users.noreply.github.com>
2021-12-01 11:46:11 -0500

    improved regex buttons, alerts, etc

--- scripts/test.js
+++ scripts/test.js
@@ -5,14 +5,19 @@ $("#clear").on("click", () => {
   $("#result").val("");
   $("#copy-result").prop("disabled", true);
   showAlert("clearmsg");
+  document.getElementById("clear").innerHTML = "Cleared!";
+  setTimeout(function(){ document.getElementById("clear").innerHTML = "Clear"; }, 2000);
 });
 
-function copyText(selector) {
+function copyText(selector, button) {
   const el = $(selector);
+  const btn = $(button);
   el.select();
   document.execCommand("copy");
   el.blur();
   document.getSelection().removeAllRanges();
+  document.getElementById(button).innerHTML = "Copied!";
+  setTimeout(function(){ document.getElementById(button).innerHTML = "Copy"; }, 2000);
   showAlert("copymsg");
 }
 
@@ -24,14 +29,28 @@ function showAlert(id) {
 
 function runRegex() {
   let input = document.getElementById("regexInput").value
+  let runError = document.getElementById("runError");
+  let runSuccess = document.getElementById("runSuccess");
   if (input.length == 0) {
     showAlert("emptymsg");
+    runSuccess.className = "";
+    runError.className = "fas fa-times";
+    setTimeout(function(){ runError.className = runError.className.replace("fas fa-times", ""); }, 2000);
   } else {
-    let output = input.replace(/test/gi, 'test successful!');
+    let output = input.replace(/    "|",?/gi, '');
+    output = output.replace(/§/gi, '&');
+    output = output.replace(/(&[ol].*?)(&[a-f1-9]|\n)/gis, '$1&r$2');
+    output = output.replace(/\n&r/gi, '&r\n');
+    output = output.replace(/&[a-f1-9]&([a-f1-9])/gi, '&$1');
+    output = output.replace(/\\/gi, '"');
+    output = output.replace(/\n &8\[/gi, '\n &8[');//em space
+    runError.className = "";
+    runSuccess.className = "fas fa-check";
+    setTimeout(function(){ runSuccess.className = runSuccess.className.replace("fas fa-check", ""); }, 2000);
     $("#result").val(output);
     $("#copy-result").prop("disabled", false);
     $("#copy-result").on("click", () => {
-      copyText("#result");
+      copyText("#result", "copy-result");
     });
   }
 }
