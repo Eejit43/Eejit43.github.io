69e201a00be9111b58e641334e6ba5fad2a03c1d
Eejit <76887639+Eejit43@users.noreply.github.com>
2021-12-02 17:07:10 -0500

    testing clipboard display

---
+++ scripts/quick-copy.js
@@ -0,0 +1,50 @@
+function showAlert(id) {
+  var element = document.getElementById(id);
+  element.className = "alert show";
+  setTimeout(function () {
+    element.className = element.className.replace("alert show", "alert");
+  }, 2000);
+}
+
+navigator.clipboard.readText()
+  .then(text => {
+    $("#copiedtext").val(text);
+    document.getElementById("clipboardwarning").innerHTML = "";
+  })
+  .catch(err => {
+  if (err.toString().match(/focused/g)) {
+  document.getElementById("clipboardwarning").innerHTML = "<i class='fas fa-exclamation-triangle'></i> Tab not focused, unable to read clipboard!<br>";
+  } else if (err.toString().match(/denied/g)) {
+  document.getElementById("clipboardwarning").innerHTML = "<i class='fas fa-exclamation-triangle'></i> Permission to read clipboard denied!<br>";
+  } else {
+  document.getElementById("clipboardwarning").innerHTML = "<i class='fas fa-exclamation-triangle'></i> Unable to read clipboard!<br>";
+  }
+  })
+
+setInterval(async () => {
+  navigator.clipboard.readText()
+  .then(text => {
+    if (text.length === 0) {
+      $("#copiedtext").val("");
+    $("#selectclipboard").prop("disabled", true);
+    document.getElementById("clipboardwarning").innerHTML = "<span style='color:#009c3f;'><i class='far fa-clipboard'></i> Your clipboard is empty!<br></span>";
+    } else {
+      $("#copiedtext").val(text);
+    document.getElementById("clipboardwarning").innerHTML = "";
+    let btn = $("#selectclipboard");
+    btn.prop("disabled", false);
+    btn.on("click", () => {
+      $("#copiedtext").select();
+    });
+  }
+  })
+  .catch(err => {
+  if (err.toString().match(/focused/g)) {
+  document.getElementById("clipboardwarning").innerHTML = "<i class='fas fa-exclamation-triangle'></i> Tab not focused, unable to read clipboard!<br>";
+  } else if (err.toString().match(/denied/g)) {
+  document.getElementById("clipboardwarning").innerHTML = "<i class='fas fa-exclamation-triangle'></i> Permission to read clipboard denied!<br>";
+  } else {
+  document.getElementById("clipboardwarning").innerHTML = "<i class='fas fa-exclamation-triangle'></i> Unable to read clipboard!<br>";
+  }
+  })
+}, 3000);
