461ef88216233b8e0f0abb4905d64605dd45265a
Eejit <76887639+Eejit43@users.noreply.github.com>
2022-01-17 16:13:13 -0500

    added support for other space determiners + fixed regex

--- scripts/morse-code-converter.js
+++ scripts/morse-code-converter.js
@@ -6,13 +6,39 @@ let fromMorseBtn = document.getElementById('from-morse');
 let clearBtn = document.getElementById('clear');
 let result = document.getElementById('result');
 let resultCopy = document.getElementById('copy-result');
+let resultCopy2 = document.getElementById('copy-result-2');
+let resultCopy3 = document.getElementById('copy-result-3');
+
+let resultVar1, resultVar2, resultVar3;
 
 window.onload = function () {
   toMorseBtn.addEventListener('click', toMorse);
   fromMorseBtn.addEventListener('click', fromMorse);
   clearBtn.addEventListener('click', clear);
   resultCopy.addEventListener('click', function () {
-    copyText('result', 'copy-result')
+    copyVar('resultVar1', 'copy-result', 'Copy')
+  });
+  resultCopy2.addEventListener('click', function () {
+    copyVar('resultVar2', 'copy-result-2', 'Copy with vertical slash space')
+  });
+  resultCopy3.addEventListener('click', function () {
+    copyVar('resultVar3', 'copy-result-3', 'Copy with three space space')
+  });
+}
+
+function copyVar(variable, button, message) {
+  let oldElement = document.getElementById(button);
+  let newElement = oldElement.cloneNode(true);
+  oldElement.parentNode.replaceChild(newElement, oldElement);
+  navigator.clipboard.writeText(eval(variable));
+  newElement.innerHTML = 'Copied!';
+  setTimeout(function () {
+    newElement.innerHTML = message;
+  }, 2000);
+  showAlert('Copied!', 'success');
+
+  newElement.addEventListener('click', function () {
+    copyText(variable, button, message)
   });
 }
 
@@ -22,6 +48,8 @@ function clear() {
   result.value = '';
   resultCopy.innerHTML = 'Copy';
   resultCopy.disabled = true;
+  resultCopy2.disabled = true;
+  resultCopy3.disabled = true;
   showAlert('Cleared!', 'success')
 }
 
@@ -142,18 +170,25 @@ const fromMorseRef = {
 const convertToMorse = (str) => {
   return str.toLowerCase().split('').map(el => {
     return toMorseRef[el] ? toMorseRef[el] : el;
-  }).join(' ');
+  }).join(' ').replace('   ', ' / ');
 };
 
 function toMorse() {
   resultCopy = document.getElementById('copy-result');
-  if (/^[ a-zA-z0-9\.,\?\'!\/\(\)&:;=\+-_"\$@]*$/.test(input.value.trim())) {
+  if (/^[ a-zA-Z0-9\.,\?\'!\/\(\)&:;=+\-_"\$@]*$/.test(input.value.trim())) {
     result.value = convertToMorse(input.value);
+    resultVar1 = convertToMorse(input.value);
+    resultVar2 = convertToMorse(input.value).replace(' / ', ' | ');
+    resultVar3 = convertToMorse(input.value).replace(' / ', '   ');
     resultCopy.disabled = false;
+    resultCopy2.disabled = false;
+    resultCopy3.disabled = false;
     showResult('e', 'success');
   } else {
     result.value = ''
     resultCopy.disabled = true;
+    resultCopy2.disabled = true;
+    resultCopy3.disabled = true;
     showResult('e', 'error');
     showAlert('Input cannot be converted into morse code!', 'error');
   }
@@ -161,7 +196,7 @@ function toMorse() {
 
 function decodeMorse(morseCode) {
   return morseCode
-    .split('   ')
+    .split(/ {2,}| *[\|\/] */)
     .map(
       a => a
       .split(' ')
@@ -174,13 +209,18 @@ function decodeMorse(morseCode) {
 function fromMorse() {
   let inputVal = input.value.trim().replace(/_/g, '-');
   resultCopy = document.getElementById('copy-result');
-  if (/^[.-]{1,7}( [.-]{1,7})*(   [.-]{1,7}( [.-]{1,7})*)*$/g.test(inputVal)) {
+  if (/^[.-]{1,7}( [.-]{1,7})*(( {2,}| *[\|\/] *)[.-]{1,7}( [.-]{1,7})*)*$/g.test(inputVal)) {
     result.value = decodeMorse(inputVal);
+    resultVar1 = decodeMorse(inputVal);
     resultCopy.disabled = false;
+    resultCopy2.disabled = true;
+    resultCopy3.disabled = true;
     showResult('d', 'success');
   } else {
     result.value = ''
     resultCopy.disabled = true;
+    resultCopy2.disabled = true;
+    resultCopy3.disabled = true;
     showResult('d', 'error');
     showAlert('Invalid morse code!', 'error');
   }
